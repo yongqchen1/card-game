74a41dfa51d6d797077c4acf37060932a3599f86
bb <ychen8824@bths.edu>
2020-07-07 22:35:40 -0400

    a

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -1,6 +1,6 @@
-import Card from '../objects/card';
 import io from 'socket.io-client';
-import Dealer from '../objects/dealer';
+import Card from '../objects/card';
+import Dealer from "../objects/dealer";
 import Zone from '../objects/zone';
 
 export default class Game extends Phaser.Scene {
@@ -11,15 +11,9 @@ export default class Game extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('purplewizard','../src/assets/purplewizard.png');
-        this.load.image('background','../src/assets/background.png');
-        this.load.image('back','../src/assets/back.png')
-
-
-        this.load.image('cyanCardFront', '../src/assets/CyanCardFront.png');
-        this.load.image('cyanCardBack', '../src/assets/CyanCardBack.png');
-        this.load.image('magentaCardFront', '../src/assets/MagentaCardFront.png');
-        this.load.image('magentaCardBack', '../src/assets/MagentaCardBack.png');
+        this.load.image('purplewizard','src/assets/purplewizard.png');
+        this.load.image('background','src/assets/background.png');
+        this.load.image('back','src/assets/back.png')
     }
 
     create() {
